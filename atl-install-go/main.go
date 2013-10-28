40603efa698360bd258e579e084de0c2509390db
Sebastien Binet <binet@cern.ch>
2013-10-28 17:39:34 +0100

    main: use curl+tar for speed...

--- atl-install-go/main.go
+++ atl-install-go/main.go
@@ -9,16 +9,111 @@ import (
 	//"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
 )
 
 var g_odir = flag.String("o", "/afs/cern.ch/atlas/offline/external/Go", "output directory where to install the go rungime")
+var g_verbose = flag.Bool("v", false, "enable verbose printouts")
+var g_mode = flag.String("mode", "curl", "what mode to use (go|curl)")
 
-func download(version string, platform [2]string) error {
+func curl_download(version string, platform [2]string) error {
 	odir := filepath.Join(*g_odir, version, "tmp-"+platform[0]+"-"+platform[1])
 
 	// create out-dir layout
-	fmt.Printf("~~~ %s\n", odir)
+	if *g_verbose {
+		fmt.Printf("~~~ %s\n", odir)
+	}
+	err := os.MkdirAll(odir, 0755)
+	if err != nil {
+		return err
+	}
+
+	// https://go.googlecode.com/files/go1.1.2.linux-amd64.tar.gz
+	url := fmt.Sprintf(
+		"https://go.googlecode.com/files/go%s.%s-%s.tar.gz",
+		version,
+		platform[0],
+		platform[1],
+	)
+
+	curl := exec.Command(
+		"curl",
+		"-L", url,
+	)
+
+	untar := exec.Command(
+		"tar",
+		"-C", odir,
+		"-zxf",
+		"-",
+	)
+
+	if *g_verbose {
+		fmt.Printf("curl.cmd:    %v\n", curl.Args)
+		fmt.Printf("untar.cmd:   %v\n", untar.Args)
+	}
+
+	curl_out, err := curl.StdoutPipe()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "**error** curl.cmd:    %v\n", curl.Args)
+		fmt.Fprintf(os.Stderr, "**error** curl.stdout: %v\n", err)
+		return err
+	}
+
+	err = curl.Start()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "**error** curl.cmd:   %v\n", curl.Args)
+		fmt.Fprintf(os.Stderr, "**error** curl.start: %v\n", err)
+		return err
+	}
+
+	untar.Stdin = curl_out
+
+	err = untar.Start()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "**error** untar.cmd:   %v\n", untar.Args)
+		fmt.Fprintf(os.Stderr, "**error** untar.start: %v\n", err)
+		return err
+	}
+
+	err = curl.Wait()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "**error** curl.cmd:   %v\n", curl.Args)
+		fmt.Fprintf(os.Stderr, "**error** curl.start: %v\n", err)
+		return err
+	}
+
+	err = untar.Wait()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "**error** untar.cmd:  %v\n", untar.Args)
+		fmt.Fprintf(os.Stderr, "**error** untar.wait: %v\n", err)
+		return err
+	}
+
+	err = os.Rename(
+		filepath.Join(odir, "go"),
+		filepath.Join(*g_odir, version, platform[0]+"_"+platform[1]),
+	)
+	if err != nil {
+		return err
+	}
+	err = os.RemoveAll(odir)
+	if err != nil {
+		return err
+	}
+
+	goroot := filepath.Join(*g_odir, version, platform[0]+"_"+platform[1])
+	return create_setup_files(goroot, platform)
+}
+
+func go_download(version string, platform [2]string) error {
+	odir := filepath.Join(*g_odir, version, "tmp-"+platform[0]+"-"+platform[1])
+
+	// create out-dir layout
+	if *g_verbose {
+		fmt.Printf("~~~ %s\n", odir)
+	}
 	err := os.MkdirAll(odir, 0755)
 	if err != nil {
 		return err
@@ -59,7 +154,9 @@ func download(version string, platform [2]string) error {
 		switch hdr.Typeflag {
 		case tar.TypeDir:
 			dir := filepath.Join(odir, hdr.Name)
-			fmt.Printf(">>> %s\n", dir)
+			if *g_verbose {
+				fmt.Printf(">>> %s\n", dir)
+			}
 			err = os.MkdirAll(dir, 0755)
 			if err != nil {
 				return err
@@ -73,7 +170,9 @@ func download(version string, platform [2]string) error {
 			return err
 		}
 		oname := filepath.Join(odir, hdr.Name)
-		fmt.Printf("::: %s (%s)\n", oname, string(byte(hdr.Typeflag)))
+		if *g_verbose {
+			fmt.Printf("::: %s (%s)\n", oname, string(byte(hdr.Typeflag)))
+		}
 		dir := filepath.Dir(oname)
 		err = os.MkdirAll(dir, 0755)
 		if err != nil {
@@ -109,6 +208,10 @@ func download(version string, platform [2]string) error {
 	}
 
 	goroot := filepath.Join(*g_odir, version, platform[0]+"_"+platform[1])
+	return create_setup_files(goroot, platform)
+}
+
+func create_setup_files(goroot string, platform [2]string) error {
 	// create setup.sh
 	setup_sh, err := os.Create(
 		filepath.Join(goroot, "setup.sh"),
@@ -130,7 +233,7 @@ export PATH=${GOPATH}/bin:${PATH}
 
 	// create setup.csh
 	setup_csh, err := os.Create(
-		filepath.Join(*g_odir, version, platform[0]+"_"+platform[1], "setup.csh"),
+		filepath.Join(goroot, "setup.csh"),
 	)
 	if err != nil {
 		return err
@@ -176,16 +279,41 @@ $ atl-install-go [options] <go-version>
 		{"linux", "386"},
 	}
 
+	type download_fct func(version string, platform [2]string) error
+	download := go_download
+
+	switch *g_mode {
+	case "curl":
+		download = curl_download
+	case "go":
+		download = go_download
+	default:
+		fmt.Fprintf(os.Stderr, "**error** invalid download mode (%s)\n", *g_mode)
+	}
+
 	all_good := true
+	errch := make(chan error, len(platforms))
 	for _, plat := range platforms {
-		fmt.Printf(":: installing %s-%s...\n", plat[0], plat[1])
-		err := download(version, plat)
+		go func(plat [2]string) {
+			fmt.Printf(":: installing %s-%s...\n", plat[0], plat[1])
+			err := download(version, plat)
+			errch <- err
+			if err != nil {
+				return
+			}
+			fmt.Printf(":: installing %s-%s... [ok]\n", plat[0], plat[1])
+		}(plat)
+	}
+
+	for i := 0; i < len(platforms); i++ {
+		err := <-errch
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "**error** %v\n", err)
 			all_good = false
-			continue
+			fmt.Fprintf(os.Stderr, "**error** %v\n", err)
 		}
-		fmt.Printf(":: installing %s-%s... [ok]\n", plat[0], plat[1])
+		if i == len(platforms)-1 {
+			close(errch)
+		}
 	}
 
 	if !all_good {
