7b92118e66b153f96255f70705bce065b71b0429
Sebastien Binet <binet@cern.ch>
2013-11-02 11:58:27 +0100

    main: use cmt.TagDiff

--- atl-get-tag-diff/main.go
+++ atl-get-tag-diff/main.go
@@ -3,10 +3,36 @@ package main
 
 import (
 	"fmt"
+	"flag"
+	"os"
+
+	"github.com/atlas-org/cmt"
 )
 
 func main() {
-	fmt.Printf(":: get-tag-diff\n")
+	v := flag.Bool("v", false, "enable verbose mode")
+	flag.Parse()
 
-	fmt.Printf(":: bye.\n")
+	old := ""
+	new := ""
+
+	switch flag.NArg() {
+	default:
+		fmt.Fprintf(os.Stderr, "**error** you need to give 2 releases/nightlies setup-strings\n")
+		flag.Usage()
+		os.Exit(1)
+	case 2:
+		old = flag.Args()[0]
+		new = flag.Args()[1]
+	}
+
+	diffs, err := cmt.TagDiff(old, new, *v)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "**error** %v\n", err)
+		os.Exit(1)
+	}
+	if len(diffs) > 0 {
+		os.Exit(1)
+	}
+	os.Exit(0)
 }
